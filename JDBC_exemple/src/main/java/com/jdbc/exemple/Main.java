3b1cd7ebf528088a2f61e249fe022b5afcd31741
Ihor <Ihor@10.7.0.100>
2017-08-28 01:24:26 +0300

    some JDBC example

---
+++ JDBC_exemple/src/main/java/com/jdbc/exemple/Main.java
@@ -0,0 +1,64 @@
+package com.jdbc.exemple;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/team_db";
+		String username = "root";
+		String password = "toor";
+		
+		Connection conn = null;
+		
+		try {
+			
+//			Class.forName("com.mysql.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connection open");
+			
+			
+//			Statement st = conn.createStatement();
+//			st.execute("CREATE TABLE person (id int PRIMARY KEY AUTO_INCREMENT, name varchar(255))");
+//			st.executeUpdate("insert into person (name) values ('Ivan')");
+//			st.close();
+			
+//			PreparedStatement ps = null;
+//			ps = conn.prepareStatement("insert into person (name) values (?)");
+//			ps.setString(1, "Ihor");
+//			ps.executeUpdate();
+//			ps.setString(1, "Bob");
+//			ps.executeUpdate();
+			
+//			ps = conn.prepareStatement("select * from person");
+//			ResultSet rs = ps.executeQuery();
+//			while(rs.next()){
+//				System.out.println("Name " + rs.getString(2));
+//			}
+//			
+//			ps.close();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			if(conn != null){
+				try {
+					conn.close();
+					System.out.println("Connection close");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+}
